b40df58050b27f3cb22d3278e56a36c025601917
Neil Patil <neilpatil215@gmail.com>
2019-01-19 17:13:20 -0600

    Add CastleTalkUtils

---
+++ bot/src/bc19/CastleTalkUtils.java
@@ -0,0 +1,31 @@
+package bc19;
+
+public class CastleTalkUtils {
+
+    private static final int INSTRUCTION_SIZE_BITS = 2;
+    private static final int ARGUMENT_SIZE_BITS = 6;
+
+    private static final byte CASTLE_LOCATION_COMMUNICATION_MASK = (byte) (0b11 << ARGUMENT_SIZE_BITS);
+
+    private static void sendCastleTalk(MyRobot r, byte message) {
+        r.castleTalk(message);
+    }
+
+    private static boolean instructionMatches(byte instructionMask, int castleTalk) {
+        return instructionMask >>> ARGUMENT_SIZE_BITS == ((byte) castleTalk) >>> ARGUMENT_SIZE_BITS;
+    }
+
+    public static int getCastleCoord(MyRobot r, Robot other) {
+        if (instructionMatches(CASTLE_LOCATION_COMMUNICATION_MASK, other.castle_talk)) {
+            return other.castle_talk % 64;
+        }
+        return -1;
+    }
+
+    // Send either an X or Y coordinate.
+    public static void sendCastleCoord(MyRobot r, int coordinate) {
+        byte message = (byte) (CASTLE_LOCATION_COMMUNICATION_MASK | ((byte) coordinate));
+        sendCastleTalk(r, message);
+    }
+
+}
