ab9f98c63467b195becd8fde3264ff553af2220b
Neil Patil <neilpatil215@gmail.com>
2019-01-19 16:06:53 -0600

    Remove hardcoded constants in RobotSort

--- bot/src/bc19/Constants.java
+++ bot/src/bc19/Constants.java
@@ -9,4 +9,12 @@ public class Constants {
 
 	/* Pilgrim-specific */
 	public static final int PILGRIM_MINE_FUEL_COST = 1;
+
+
+	public static final int CASTLE_UNIT_TYPE = 0;
+	public static final int CHURCH_UNIT_TYPE = 1;
+	public static final int PILGRIM_UNIT_TYPE = 2;
+	public static final int CRUSADER_UNIT_TYPE = 3;
+	public static final int PROPHET_UNIT_TYPE = 4;
+	public static final int PREACHER_UNIT_TYPE = 5;
 }
