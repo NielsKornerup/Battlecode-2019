fd5af9df70b2fa7ddef3dac7f0f206a9f3f6effa
Niels <nielsskornerup@gmail.com>
2019-01-19 18:42:52 -0600

    gave time to build churches

--- bot/src/bc19/Castle.java
+++ bot/src/bc19/Castle.java
@@ -159,24 +159,14 @@ public class Castle {
             }
         }
 
-        // 2. Build a pilgrim if there are none in vision radius.
-        int numPilgrims = Utils.getUnitsInRange(r, r.SPECS.PILGRIM, true, 0, Utils.mySpecs(r).VISION_RADIUS).size();
-		// This operates off of the assumption that if there are no pilgrims in the vision radius, then they are dead.
-		// This assumption is OK because the current map generation algorithm always puts resource deposits next to castles.
-		// However, it's not good anymore if maps are handmade, or we modify pilgrims to go for resource deposits farther than that.
-        if (numPilgrims < 1) {
-            BuildAction action = Utils.tryAndBuildInRandomSpace(r, r.SPECS.PILGRIM);
-            if (action != null) {
-                return action;
-            }
-        }
-
         // TODO implement logic/heuristics to prevent existing units from starving Castle of building opportunities
 
         // 3. Build a prophet.
-        BuildAction action = Utils.tryAndBuildInRandomSpace(r, r.SPECS.PROPHET);
-        if (action != null) {
-            return action;
+        if(r.turn > 50) {
+        	BuildAction action = Utils.tryAndBuildInRandomSpace(r, r.SPECS.PROPHET);
+        	if (action != null) {
+            	return action;
+        	}
         }
 
         // 4. Finally, if we cannot build anything, attack if there are enemies in range.
